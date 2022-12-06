c6c6c63518c89035c8850497cc6675d54d466ab6
SweWill <119424405+SweWill@users.noreply.github.com>
2022-12-06 20:09:51 +0100

    Mobile navigation, about, contact, sideNav icons

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,14 @@ document.querySelector('.podCard').addEventListener("click", popup);
 function popup () {
     document.getElementById('podCard').classList.toggle("popup");
 }
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
